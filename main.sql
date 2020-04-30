ae53e5763e6eb86561a35bcb3dfeb8f3a21c7cbb
Ramos159 <edwinramos269@gmail.com>
2020-04-29 21:46:12 -0400

    SQL writing conventions enforced

--- main.sql
+++ main.sql
@@ -1,74 +1,74 @@
-CREATE TABLE users(
-    id INTEGER NOT NULL PRIMARY KEY,
-    discord_id BIGINT NOT NULL,
-    summoner_name TEXT
+CREATE TABLE Users(
+    id INTEGER PRIMARY KEY,
+    discordId BIGINT NOT NULL,
+    summonerName TEXT
 );
 
-CREATE TABLE server_settings(
-    id INTEGER NOT NULL PRIMARY KEY,
-    discord_id BIGINT NOT NULL,
+CREATE TABLE ServerSettings(
+    id INTEGER PRIMARY KEY,
+    discordId BIGINT NOT NULL,
     prefix TEXT
 );
 
-CREATE TABLE mod_users(
-    id INTEGER NOT NULL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    server_id BIGINT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(server_id) REFERENCES server_settings(id)
+CREATE TABLE ModUsers(
+    id INTEGER PRIMARY KEY,
+    userId BIGINT NOT NULL,
+    serverId BIGINT NOT NULL,
+    FOREIGN KEY(userId) REFERENCES Users(id),
+    FOREIGN KEY(serverId) REFERENCES ServerSettings(id)
 );
 
-CREATE TABLE tournaments(
-    id INTEGER NOT NULL PRIMARY KEY,
+CREATE TABLE Tournaments(
+    id INTEGER PRIMARY KEY,
     name TEXT NOT NULL
 );
 
-CREATE TABLE teams(
-    id INTEGER NOT NULL PRIMARY KEY,
-    name TEXT
+CREATE TABLE Teams(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
 -- teams in a tournament
-CREATE TABLE tournament_teams(
+CREATE TABLE TournamentTeams(
     id INTEGER NOT NULL PRIMARY KEY,
-    team_id INTEGER NOT NULL,
-    tournament_id INTEGER NOT NULL
+    teamId INTEGER NOT NULL,
+    tournamentId INTEGER NOT NULL
 );
 
 -- tracks a players stats in a specific tournament
-CREATE TABLE tournament_league_stats(
-    id INTEGER NOT NULL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    tournament_id INTEGER NOT NULL,
-    total_tournament_kills INTEGER,
-    total_tournament_deaths INTEGER,
-    total_tournament_assists INTEGER,
-    total_game_count INTEGER,
-    FOREIGN KEY (tournament_id) REFERENCES tournaments(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE TournamentLeagueStats(
+    id INTEGER PRIMARY KEY,
+    userId BIGINT NOT NULL,
+    tournamentId INTEGER NOT NULL,
+    totalTournamentKills INTEGER,
+    totalTournamentDeaths INTEGER,
+    totalTournamentAssists INTEGER,
+    totalGameCount INTEGER,
+    FOREIGN KEY (tournamentId) REFERENCES Tournaments(id),
+    FOREIGN KEY (userId) REFERENCES Users(id)
 );
 
 -- regular team roster
-CREATE TABLE tournament_league_teams(
-    id INTEGER NOT NULL PRIMARY KEY,
-    team_id INTEGER,
-    tournament_id INTEGER,
-    top_laner BIGINT,
+CREATE TABLE TournamentLeagueTeams(
+    id INTEGER PRIMARY KEY,
+    teamId INTEGER,
+    tournamentId INTEGER,
+    topLaner BIGINT,
     jungler BIGINT,
-    mid_laner BIGINT,
+    midLaner BIGINT,
     adc BIGINT,
     support BIGINT,
     sub1 BIGINT,
     sub2 BIGINT,
     sub3 BIGINT,
-    FOREIGN KEY(team_id) REFERENCES tournament_teams(id),
-    FOREIGN KEY(tournament_id) REFERENCES tournaments(id),
-    FOREIGN KEY(top_laner) REFERENCES users(id),
-    FOREIGN KEY(jungler) REFERENCES users(id),
-    FOREIGN KEY(mid_laner) REFERENCES users(id),
-    FOREIGN KEY(adc) REFERENCES users(id),
-    FOREIGN KEY(support) REFERENCES users(id),
-    FOREIGN KEY(sub1) REFERENCES users(id),
-    FOREIGN KEY(sub2) REFERENCES users(id),
-    FOREIGN KEY(sub3) REFERENCES users(id)
+    FOREIGN KEY(teamId) REFERENCES TournamentTeams(id),
+    FOREIGN KEY(tournamentId) REFERENCES Tournaments(id),
+    FOREIGN KEY(topLaner) REFERENCES Users(id),
+    FOREIGN KEY(jungler) REFERENCES Users(id),
+    FOREIGN KEY(midLaner) REFERENCES Users(id),
+    FOREIGN KEY(adc) REFERENCES Users(id),
+    FOREIGN KEY(support) REFERENCES Users(id),
+    FOREIGN KEY(sub1) REFERENCES Users(id),
+    FOREIGN KEY(sub2) REFERENCES Users(id),
+    FOREIGN KEY(sub3) REFERENCES Users(id)
 );
